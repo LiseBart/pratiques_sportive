4d7872c8b8f615d0eac143a7444cd16078e06535
lise.bartholus <lise.bartholus@mnhn.fr>
2024-12-06 11:30:47 +0100

    set make

--- make.R
+++ make.R
@@ -1,5 +1,23 @@
-# charger les dépendances
+# Install devtools if needed
+if (system.file(package = 'devtools') == "") {
+  install.packages("devtools", repos = "https://pbil.univ-lyon1.fr/CRAN/")
+}
 
+# Install packages if needed
+devtools::install_deps()
+
+# Attach packages
 devtools::load_all()
 `%>%` = magrittr::`%>%`
+
+#RUN PROJECT
+
+# Load data
+
+# Do you need to download data ?
+Download <- FALSE # If TRUE, data will be downloaded, tap FALSE if data is available in /data
+
+source(here::here("analyses","01-Load_data.R"))
+
+source(here::here("analyses","04-Exploration_lise.R"))
 
