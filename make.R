6fd3de2db2299bf7c22d2d5d1fa691067a873500
Quentin Guilloit <q.guilloit@gmail.com>
2024-12-06 11:09:07 +0100

    reset make sur celui de lise

--- make.R
+++ make.R
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-# Setup project ----
-
-## Install packages ----
-
-devtools::install_deps(upgrade = "never")
-
-
-## Load packages & functions ----
-
-# plus utiliserlaod_all car pas compatible avec target 
-# devtools::load_all()
-`%>%` = magrittr::`%>%`
-
-
-
-# Run project ----
-
-
-## Make report ----
-
-#quarto::quarto_render(input          = here::here("index.qmd"), 
-#                      execute_params = list(mammals_family = "Canidae"))
-
-## targets ----
-# ---- project execution
-targets::tar_config_set(
-  store  = "outputs/pipeline/",
-  script = "_targets.R"
-)
-
-targets::tar_make()
-targets::tar_visnetwork()
-=======
 # Install devtools if needed
 if (system.file(package = 'devtools') == "") {
   install.packages("devtools", repos = "https://pbil.univ-lyon1.fr/CRAN/")
@@ -54,5 +20,3 @@ Download <- FALSE # If TRUE, data will be downloaded, tap FALSE if data is avail
 source(here::here("analyses","01-Load_data.R"))
 
 source(here::here("analyses","04-Exploration_lise.R"))
-
->>>>>>> origin/main
