17ef5d95b60dafbbf02522f694a9710d5455c129
Quentin Guilloit <q.guilloit@gmail.com>
2024-12-06 10:01:06 +0100

    figure quentin fini correctif

--- analyses/02-type_sport_hf.R
+++ analyses/02-type_sport_hf.R
@@ -35,30 +35,6 @@ data_quentin <- data_quentin %>%
   dplyr::arrange(-diff)
 
 
-#Plot all sport 
-
-ggplot2::ggplot(data_quentin, ggplot2::aes(x = diff, y = diff, label = Fédération)) +
-  ggplot2::geom_point(color = "blue", size = 3) +    # Points
-  ggplot2::geom_text(nudge_x = 0.5, nudge_y = 0.5, color = "red") + 
-  ggplot2::theme_minimal()
-
-
-# Créer un nuage de points
-plot(1:nrow(data_quentin), data_quentin$diff, 
-     type = "p",  # Points
-     main = "Nuage de points", 
-     xlab = "Ordre des données", 
-     ylab = "Col2 (axe y)", 
-     xlim = c(0, nrow(data_quentin) + 20), 
-     pch = 19, col = "blue")
-
-# Ajouter les étiquettes (Col1) aux points
-selectionsport <- c(1,6,10,12,18,40,56,70,81,86,92,100,106)
-text(selectionsport, data_quentin$diff[selectionsport], labels = data_quentin$Fédération[selectionsport], pos = 4, cex = 0.8, col = "red")
-
-
-
-
 # Créer un graphique avec ggplot2
 p <- ggplot2::ggplot(data_quentin, ggplot2::aes(x = 1:nrow(data_quentin), y = diff)) +
   ggplot2::geom_point(color = "dodgerblue", size = 3, alpha = 0.7) + # Points bleus translucides
